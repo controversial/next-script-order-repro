11e97678f722b085e16be67fe520b19715a8aa43
Luke Taylor <luke@deentaylor.com>
2024-11-27 12:36:13 -0500

    Next.js script order reproduction

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,28 @@
+import Script from 'next/script';
+
 export default function RootLayout({ children }) {
   return (
     <html>
-      <head />
+      <head>
+        <Script
+          strategy="beforeInteractive"
+          id="inject-polyfill"
+        >
+          {`
+            if (!(
+              Array.prototype.toSorted
+              && Array.prototype.with
+            ) || true /* for testing */) {
+              console.log('injecting polyfill script');
+              const newScript = document.createElement('script');
+              newScript.src = 'https://cdnjs.cloudflare.com/polyfill/v3/polyfill.min.js?version=4.8.0&features=es2022%2Ces2023';
+              newScript.onload = () => { console.log('polyfill script loaded, *after* component mounted\\nEven though it was injected as a blocking script at the beginning of <head>, it didn’t get a chance to block loading Next.js chunks, because it wasn’t injected until *after* Next.js chunk scripts had already started loading'); };
+              document.head.prepend(newScript);
+            }
+          `}
+        </Script>
+      </head>
+
       <body>{children}</body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,10 @@
+'use client';
+
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null;
+  console.log('client component mounted');
+
+  // imagine using polyfilled funciton here as part of render
+
+  return <div>hey</div>;
 }
